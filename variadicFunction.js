c70548f09a8020006411fd68c54536bdf93be9a0
devfabien <ish001fabi@gmail.com>
2023-08-08 19:50:55 +0200

    a func that return array of ordered even numbers

--- variadicFunction.js
+++ variadicFunction.js
@@ -3,7 +3,13 @@
 // }
 // console.log(add(2,34))
 
-function add(...rest){
-    return rest.reduce((a,b)=>a+b,0)
+// function add(...rest){
+//     return rest.reduce((a,b)=>a+b,0)
+// }
+// console.log(add(1,2,3,4,6))
+
+let check=(arr)=>{
+    
+    return arr.map(a=>a+1).sort((a,b)=>a-b)
 }
-console.log(add(1,2,3,4,6))
+console.log(check([8,1,7,3,4,5,6]))
