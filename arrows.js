beb1cfa40ec266547aa62abb5f53f4bb74c68e48
devfabien <ish001fabi@gmail.com>
2023-08-08 20:22:42 +0200

    map that returns a number

--- arrows.js
+++ arrows.js
@@ -1,3 +1,6 @@
-let check=()=> ({user:"olivier"});
+// let check=()=> ({user:"olivier"});
 
-console.log(check())
+// console.log(check())
+const check=(arr)=> arr.map((a)=>a%2==0 ?a*2:a)
+
+console.log(check([1,2,3,4,5,6]))
