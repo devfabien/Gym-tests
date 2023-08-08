7010ff8a12cbf6b77f2b7c074fd4a10d9c2f7e25
devfabien <ish001fabi@gmail.com>
2023-08-08 19:38:04 +0200

    with rest parameter

--- variadicFunction.js
+++ variadicFunction.js
@@ -1,4 +1,9 @@
-function add(){
-    return Array.prototype.slice.call(arguments).reduce((a,b)=>a+b,0)
+// function add(){
+//     return Array.prototype.slice.call(arguments).reduce((a,b)=>a+b,0)
+// }
+// console.log(add(2,34))
+
+function add(...rest){
+    return rest.reduce((a,b)=>a+b,0)
 }
-console.log(add(2,34))
+console.log(add(1,2,3,4,6))
